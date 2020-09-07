935e7bea3fe0369f2d4ce598ca6c7c65ef2681c1
Suresh SK <suresh.uncbc.coding@gmail.com>
2020-09-07 19:59:40 -0400

    initial version of server.js, models so nodemon can be run

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const db = require("./models");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populate", { useNewUrlParser: true });
+
+app.post("/api/workouts", ({ body }, res) => {
+  db.Workout.create({})
+    .then(dbWorkout => {
+      console.log(dbWorkout);
+    })
+    .catch(({ message }) => {
+      console.log(message);
+    });
+});
+
+app.put("/api/workouts", ({ body }, res) => {
+  db.Workout.create(body)
+    .then(({ _id }) => db.Workout.findOneAndUpdate({}, { $push: { exercises: _id } }, { new: true }))
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+app.get("/api/workouts/range", (req, res) => {
+  db.Workout.find({}).limit(7)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+
+
+app.get("/api/workouts", (req, res) => {
+  db.Workout.find({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
